5d91c5b2470210e8746e10a49f74d0bf647fcca1
Ibrahim Quraishi <quraishi@mcmaster.ca>
2024-04-08 13:04:33 -0400

    Updated user info

--- app/src/main/java/com/example/keychat/UserInfo.java
+++ app/src/main/java/com/example/keychat/UserInfo.java
@@ -2,6 +2,16 @@ package com.example.keychat;
 
 public class UserInfo {
     private static String username;
+    private static String userID;
+
+    public static String getUserID() {
+        return userID;
+    }
+
+    public static void setUserID(String userID) {
+        UserInfo.userID = userID;
+    }
+
 
     public static void setUsername(String user) {
         username = user;
